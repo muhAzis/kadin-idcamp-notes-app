95f49aa3ca2cf9ce90b172a10487cdc7c4216164
azis <muhabdulazis555@gmail.com>
2023-05-14 16:17:10 +0700

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+// Import our custom CSS
+import '../scss/main.scss';
+import swRegister from './utils/sw-register';
+
+import App from './views/app';
+import * as bootstrap from 'bootstrap';
+
+const app = new App({
+  content: document.getElementById('mainContent'),
+  navListContainer: document.querySelectorAll('#navListContainer, #navListOffcanvas'),
+});
+
+window.addEventListener('hashchange', () => {
+  app.renderPage();
+});
+
+window.addEventListener('load', async () => {
+  app.renderPage();
+
+  await swRegister();
+});
